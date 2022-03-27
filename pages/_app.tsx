8aabd9ed6c8fa15891ec377a257a29da71afbb46
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-03-27 20:12:16 +0530

    feat: added meta tags & hybrid amp flag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app';
+import { MetaInformation } from '../components/MetaInformation';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MetaInformation />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
